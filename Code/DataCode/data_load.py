3ac88e6ee8053dce9bfd47cbae39acb638dca448
Aryan-Dessai-25 <125971800+Aryan-Dessai-25@users.noreply.github.com>
2024-06-10 04:33:59 +0530

    Update data_load.py

--- Code/DataCode/data_load.py
+++ Code/DataCode/data_load.py
@@ -1,9 +1,15 @@
 import numpy as np
-from Utils.utils import 
+from Utils.utils import minmaxnorm, create_mask, bar_tensor, pie_tensor
 from DataCode.data_utils import data_load_magico4
 
-def dataloader(data_folder='magico4',plot_type='pie',drop_rate=0.5):
+def dataloader(data_folder='magico4',drop_rate=0.5,seed=42):
     
     X,Y=data_load_magico4(data_folder)
-    if(plot_type=='bar'):
+    num_inst=X.shape[0]
+    num_feats=X.shape[1]
+    mat_mask, mat_rev_mask=create_mask(num_inst,num_feats,drop_rate=0.5,seed)
+    X_haphazard=np.multiply(X,mat_mask)
+
+return X_haphazard, Y, mat_rev_mask
+        
         
