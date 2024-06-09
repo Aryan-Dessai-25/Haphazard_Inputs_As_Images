01be75e8a82884a5217fff1fd91f8236cbcf43f3
Aryan-Dessai-25 <125971800+Aryan-Dessai-25@users.noreply.github.com>
2024-06-09 19:13:39 +0530

    Create utils.py

---
+++ Code/Utils/utils.py
@@ -0,0 +1,11 @@
+import random
+import os
+import numpy as np
+import torch
+
+#--------------Seed--------------#
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
