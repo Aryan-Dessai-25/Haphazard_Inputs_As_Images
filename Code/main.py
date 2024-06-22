3e925689823450b909fa609e459d44c44a599ef7
Aryan-Dessai-25 <dessaiaryan4419@gmail.com>
2024-06-22 14:09:03 +0200

    added timing

--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,16 @@
 import os
 import argparse
 import random
-import numpy as np
-import torch
-import torchvision
-import timm
-import torch.optim as optim
-import torch.nn as nn
+import datetime
+import numpy as np # type: ignore
+import torch # type: ignore
+import torchvision # type: ignore
+import timm # type: ignore
+import torch.optim as optim # type: ignore
+import torch.nn as nn # type: ignore
 import sys
 import pickle
-from tqdm import tqdm
+from tqdm import tqdm # type: ignore
 sys.path.append('/code/DataCode/')
 path_to_result = "./Results/"
 
@@ -87,6 +88,12 @@ if __name__ == '__main__':
     if plot_type=='bar':
         feat=np.arange(num_feats)
 
+    minmax_time=0
+    plot_time=0
+    model_time=0
+    loss_backprop_time=0
+    predict_time=0
+
     for k in tqdm(range(num_inst)):
 
         row=drop_df[k]
@@ -94,21 +101,37 @@ if __name__ == '__main__':
         label=labels[k]
         label=torch.tensor(label)
         
+        start1=datetime.now()
         norm_row, min_arr, max_arr=utils.minmaxnorm(row,min_arr,max_arr,epsilon=1e-15)    #normalize and update min, max
+        end1=datetime.now()
+        diff1 = (end1 - start1).total_seconds()
+        minmax_time+=diff1
 
+        start2=datetime.now()
         if plot_type=='bar':
             img=utils.bar_tensor(norm_row,rev,colors,feat,dpi=56)        #obtain bar plot tensor 
         elif plot_type=='pie':
             img=utils.pie_tensor(norm_row,colors,dpi=56)
-        
+        end2=datetime.now()
+        diff2 = (end2 - start2).total_seconds()
+        plot_time+=diff2
+
+
         img, label = img.to(device), label.to(device)      #transfer to GPU
         img=torch.reshape(img,(-1,3,224,224))      #add extra dimension corresponding to batch, as required by model
         
         optimizer.zero_grad()
+        with torch.no_grad():
+            start3=datetime.now()
         outputs=model(img)
+        with torch.no_grad():
+            end3=datetime.now()
+            diff3 = (end3 - start3).total_seconds()
+            model_time+=diff3
         outputs=torch.reshape(outputs,(-1,))
     
-            
+        with torch.no_grad():
+            start4=datetime.now() 
         loss=criterion(outputs,label.float())         #compute loss
     
         loss.backward()                               
@@ -118,10 +141,17 @@ if __name__ == '__main__':
             
     
         with torch.no_grad():
-                
+
+            end4=datetime.now()
+            diff4 = (end4 - start4).total_seconds()
+            loss_backprop_time+=diff4
+
+
+            start5=datetime.now()
+
             predicted=torch.sigmoid(outputs)          # since we use BCEWithLogitsLoss, sigmoid is required for obtaining probability
             predicted=torch.round(predicted)          # get binary prediction
-                
+              
             predicted=predicted.to('cpu')
             label=label.to('cpu')
             outputs=outputs.to('cpu')                 #transfer these to cpu to evaluate using sklearn based metrics
@@ -130,9 +160,17 @@ if __name__ == '__main__':
             pred_logits+=[outputs.item()]                    #update lists
             preds.append(predicted.item())
             true.append(label.item())
+            end5=datetime.now()
+            diff5 = (end5 - start5).total_seconds()
+            predict_time+=diff5
     b_acc=eval_metrics.BalancedAccuracy(true,preds)
     auroc=eval_metrics.AUROC(true,pred_logits)
     auprc=eval_metrics.AUPRC(true,pred_logits)
     print(f'Balanced Accuracy= {b_acc}')
     print(f'AUROC score= {auroc}')
     print(f'AUPRC score= {auprc}')
+    print('norm=',minmax_time)
+    print('plot=', plot_time)
+    print('model=', model_time)
+    print('bprop=', loss_backprop_time)
+    print('predict=', predict_time)
